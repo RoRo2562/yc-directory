69732142173b244d8a8e2e85b20d6fca6a2b05a9
Rohan <rohan.sivam2562@gmail.com>
2025-04-13 17:51:23 +1000

    Learnt how to impllement nextAuth using Oauth in the NavBar, github login now working

--- auth.ts
+++ auth.ts
@@ -4,8 +4,8 @@ import GitHub from "next-auth/providers/github";
 export const { handlers,signIn,signOut,auth} = NextAuth({
     providers: [
         GitHub({
-          clientId: process.env.GITHUB_ID!,
-          clientSecret: process.env.GITHUB_SECRET!,
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
         }),
       ],
 })
